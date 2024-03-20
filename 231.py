e37c0925704b36817671b9edf85486b3fe4866c6
Haoze Chen <albertchen2017@outlook.com>
2024-03-20 11:54:39 -0700

    update

---
+++ 231.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        while n != 0:
+            if n != 1 and n % 2 != 0:
+                return False
+            n //= 2
+        return True
+
+    def test(self, n: int):
+        if self.isPowerOfTwo(n):
+            print("True")
+        else:
+            print("False")
+
+sol = Solution()
+sol.test(1)
+sol.test(16)
+sol.test(3)
+            
